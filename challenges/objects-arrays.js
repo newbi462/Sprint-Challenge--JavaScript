e6684dd92b9692ed0e971db63027f8311ed962f9
newbi462 <isthisemailfree@gmail.com>
2019-10-18 12:20:22 -0400

    Because of CONST forced FAIL MVP will do the rest

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -124,31 +124,49 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(index) {
+  displayNames.push(`Name: ${index.animal_name}, Scientific: ${index.scientific_name}`);
+});
 console.log(displayNames);
+//"Name: Jackal, asiatic, Scientific: Canis aureus."
 
 /* Request 2: .map()
 
-The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case.
+Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map( index => lowCaseAnimalNames.push(index.animal_name.toLowerCase())  );
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter()
 
-The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
+The zoos are concerned about animals with a lower population count.
+Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
 const lowPopulationAnimals = [];
+//zooAnimals.filter(  index => lowPopulationAnimals.push(index.population < 5)  );
+function dameYouforusingConstthisiswhyLETisbetter(index) {
+  if (index.population < 5) {
+    lowPopulationAnimals.push(index);
+  }
+};
+zooAnimals.filter(dameYouforusingConstthisiswhyLETisbetter);
+
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce()
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States.
+Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
 const populationTotal = 0;
+const reducer = (accumulator, currentValue) => ({population: accumulator.population + currentValue.population});
+populationTotal = zooAnimals.reduce(reducer).population;
 console.log(populationTotal);
 
 
